72904cf9fb5a5b5c747ca413b5970290ab13301b
Eileen Ho <eileenho25@gmail.com>
2017-03-02 22:37:26 -0800

    add demo login to splash page

--- frontend/components/welcome.jsx
+++ frontend/components/welcome.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, withRouter } from 'react-router';
+import WelcomeContainer from './welcome_container';
 
-const Welcome = ({store}) => {
-  return (
-    <div className="welcome-background">
-      <div className="welcome">
-        <h1>Welcome to Spatula</h1>
-        <p>A personal, online recipe storage site</p>
-        <div className="welcome-links">
-          <Link to="/login" className='login'>LOGIN</Link>
-          <br/>
-          <Link to="/signup" className='signup'>SIGN UP</Link>
+class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+    this.loginGuest = this.loginGuest.bind(this);
+  }
+
+  loginGuest(e) {
+    e.preventDefault();
+    const guest = { user: {username: "guest", password: "password"}};
+    this.props.login(guest).then(() => this.props.router.push('/profile'));
+  }
+
+  render() {
+    return (
+      <div className="welcome-background">
+        <div className="welcome">
+          <h1>Welcome to Spatula</h1>
+          <p>A personal, online recipe storage site</p>
+          <div className="welcome-links">
+            <Link to="/login" className='login'>LOGIN</Link>
+            <Link to="/signup" className='signup'>SIGN UP</Link>
+            or Take a Tour as
+            <button className="demo-form-button" onClick={ this.loginGuest }>Guest</button>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default Welcome;
+export default withRouter(Welcome);
